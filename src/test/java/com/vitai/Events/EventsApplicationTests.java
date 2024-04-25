a12e672f3d4721e2321de79ec169f5bad7c4efdd
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-25 18:20:41 -0300

    INI - Project Initialized

---
+++ src/test/java/com/vitai/Events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitai.Events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
