3e45c53eca13a4f500b063299dc70c91424f94ee
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-30 20:34:57 -0300

    ADD - Test - Create Event

---
+++ src/test/java/com/vitai/events/services/event/CreateEventUsecaseTest.java
@@ -0,0 +1,58 @@
+package com.vitai.events.services.event;
+
+import com.vitai.events.controllers.authentication.dtos.RegisterDTO;
+import com.vitai.events.controllers.event.dtos.EventDTO;
+import com.vitai.events.domain.Event;
+import com.vitai.events.domain.User;
+import com.vitai.events.domain.enums.UserRole;
+import com.vitai.events.repositories.EventRepository;
+import com.vitai.events.repositories.UserRepository;
+import com.vitai.events.usecases.auth.RegisterUsecase;
+import com.vitai.events.usecases.event.CreateEventUsecase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class CreateEventUsecaseTest {
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @InjectMocks
+    private CreateEventUsecase createEventUsecase;
+
+    @BeforeEach
+    void initMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should create a new Event with success returning 201")
+    void testCreateNewEventSuccess() {
+        EventDTO eventDTO = EventDTO.builder()
+                .name("New event")
+                .local("Location")
+                .date(Instant.now())
+                .maxCapacity(100)
+                .build();
+
+        when(this.eventRepository.save(EventDTO.dtoToEvent(eventDTO))).thenReturn(new Event());
+
+        this.createEventUsecase.execute(EventDTO.dtoToEvent(eventDTO));
+
+        verify(this.eventRepository, times(1)).save(any(Event.class));
+    }
+
+}
