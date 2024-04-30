dc7fd254370b51ae5049dbc22795003954262d27
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-30 17:37:23 -0300

    ADD - Test - Validate User Login Endpoint

--- src/main/java/com/vitai/events/usecases/auth/LoginUsecase.java
+++ src/main/java/com/vitai/events/usecases/auth/LoginUsecase.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,7 +24,7 @@ public class LoginUsecase {
     public ResponseEntity<LoginResponseDTO> execute(LoginDTO loginDTO) {
         try {
             var usernamePassword = new UsernamePasswordAuthenticationToken(loginDTO.login(), loginDTO.password());
-            var auth = this.authenticationManager.authenticate(usernamePassword);
+            Authentication auth = this.authenticationManager.authenticate(usernamePassword);
 
             var token = tokenService.generateToken((User) auth.getPrincipal());
 
---
+++ src/test/java/com/vitai/events/services/auth/LoginUsecaseTest.java
@@ -0,0 +1,74 @@
+package com.vitai.events.services.auth;
+
+import com.vitai.events.controllers.authentication.dtos.LoginDTO;
+import com.vitai.events.controllers.authentication.dtos.LoginResponseDTO;
+import com.vitai.events.domain.User;
+import com.vitai.events.usecases.auth.LoginUsecase;
+import com.vitai.events.utils.security.TokenService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class LoginUsecaseTest {
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private TokenService tokenService;
+
+    @InjectMocks
+    private LoginUsecase loginUsecase;
+
+    @BeforeEach
+    void initMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testLoginSuccess() {
+        String username = "testUser";
+        String password = "testPassword";
+
+        LoginDTO loginDTO = new LoginDTO(username, password);
+
+        User user = User.builder()
+                .login(username)
+                .password(password)
+                .build();
+
+        String token = "generated_token";
+        when(authenticationManager.authenticate(any()))
+                .thenReturn(new UsernamePasswordAuthenticationToken(user, null));
+        when(tokenService.generateToken(user)).thenReturn(token);
+
+        ResponseEntity<LoginResponseDTO> responseEntity = loginUsecase.execute(loginDTO);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(token, responseEntity.getBody().token());
+    }
+
+    @Test
+    @DisplayName("Should return UNAUTHORIZED when login fails")
+    void testLoginFailure() {
+        LoginDTO loginDTO = new LoginDTO("invalid_username", "invalid_password");
+
+        when(this.authenticationManager.authenticate(any())).thenThrow(new RuntimeException());
+
+        ResponseEntity<LoginResponseDTO> response = this.loginUsecase.execute(loginDTO);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+}
