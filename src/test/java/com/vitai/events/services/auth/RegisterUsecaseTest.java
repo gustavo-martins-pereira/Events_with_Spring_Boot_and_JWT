b288d4e530bab4d6ca7726bc614eb847002202ae
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-04-30 15:30:57 -0300

    ADD - Test - Validate User Registration Endpoint

--- src/test/java/com/vitai/events/services/auth/RegisterUsecaseTest.java
+++ src/test/java/com/vitai/events/services/auth/RegisterUsecaseTest.java
@@ -5,17 +5,18 @@ import com.vitai.events.domain.User;
 import com.vitai.events.domain.enums.UserRole;
 import com.vitai.events.repositories.UserRepository;
 import com.vitai.events.usecases.auth.RegisterUsecase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class RegisterUsecaseTest {
@@ -26,18 +27,34 @@ class RegisterUsecaseTest {
     @InjectMocks
     private RegisterUsecase registerUsecase;
 
+    @BeforeEach
+    void initMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
+    @DisplayName("Should create a new User with success returning 201 with No Body")
     void testRegisterNewUserSuccess() {
-        RegisterDTO registerDTO = RegisterDTO.builder()
-                .name("John Doe")
-                .login("john")
-                .password(new BCryptPasswordEncoder().encode("password123"))
-                .role(UserRole.USER)
-                .build();
+        RegisterDTO registerDTO = new RegisterDTO("John Doe", "john", "password", UserRole.USER);
+        when(this.userRepository.findByLogin(registerDTO.getLogin())).thenReturn(null);
 
-        var user = registerUsecase.execute(registerDTO);
+        ResponseEntity<Object> response = this.registerUsecase.execute(registerDTO);
 
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        verify(this.userRepository, times(1)).save(any(User.class));
+    }
 
+    @Test
+    @DisplayName("Should not create a new User because there is a User with same login")
+    void testRegisterNewUserWithExistingLogin() {
+        RegisterDTO registerDTO = new RegisterDTO("John Doe", "john", "password", UserRole.USER);
+        when(this.userRepository.findByLogin(registerDTO.getLogin())).thenReturn(new User());
+
+        ResponseEntity<Object> response = this.registerUsecase.execute(registerDTO);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Username already exists", response.getBody());
+        verify(this.userRepository, never()).save(any(User.class));
     }
 
 }
